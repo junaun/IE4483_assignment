8c3f492e409273c69001d70026fac963dd7523ea
junaun <jtan515@e.ntu.edu.sg>
2023-11-01 18:25:07 +0800

    update

---
+++ resnet_50.py
@@ -0,0 +1,273 @@
+import os
+import cv2
+import time
+import random
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.data import Dataset, DataLoader
+
+import torchvision.transforms as T
+from torchvision.utils import make_grid
+from torchvision.models import resnet50
+
+from sklearn.model_selection import train_test_split
+
+from PIL import Image
+
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+device = 'cuda'
+def get_train_transform():
+    return T.Compose([
+        T.RandomHorizontalFlip(p=0.5),
+        T.RandomRotation(15),
+        T.RandomCrop(204),
+        T.ToTensor(),
+        T.Normalize((0, 0, 0),(1, 1, 1))
+    ])
+    
+def get_val_transform():
+    return T.Compose([
+        T.ToTensor(),
+        T.Normalize((0, 0, 0),(1, 1, 1))
+    ])
+
+class CatDogDataset(Dataset):
+    def __init__(self, img_path, label, transforms = None):
+        super().__init__()
+        self.img_path = img_path
+        self.label = torch.tensor(label, dtype = torch.float32)
+        self.transforms = transforms
+        
+    def __getitem__(self, idx):
+        img = Image.open(self.img_path[idx])
+        img = img.resize((224, 224))
+        img = self.transforms(img)
+        label = self.label[idx]
+        return img, label
+        
+    def __len__(self):
+        return len(self.label)
+    
+def get_label(dir_list):
+    path = []
+    label = []
+    for i in dir_list:
+        for root, dir, files in os.walk(i):
+            for file in files:
+                if file.endswith("jpg"):
+                    path.append(i + file)
+                    if 'cat' in file:
+                        label.append(0)
+                    elif 'dog' in file:
+                        label.append(1)
+    return path, label
+
+train_path = ['train/cat/', 'train/dog/']
+train_image_path, train_label = get_label(train_path)
+train_dataset = CatDogDataset(img_path = train_image_path, label = train_label, transforms = get_train_transform())
+
+valid_path = ['valid/cat/', 'valid/dog/']
+valid_image_path, valid_label = get_label(train_path)
+valid_dataset = CatDogDataset(img_path = valid_image_path, label = valid_label, transforms = get_train_transform())
+
+train_data_loader = DataLoader(
+    dataset = train_dataset,
+    num_workers= 4,
+    batch_size = 16,
+    shuffle = True
+)
+
+val_data_loader = DataLoader(
+    dataset = valid_dataset,
+    num_workers= 4,
+    batch_size = 16,
+    shuffle = True
+)
+
+# test_data_loader = DataLoader(
+#     dataset = test_dataset,
+#     num_workers = 4,
+#     batch_size = 16,
+#     shuffle = True
+# )
+
+def accuracy(preds, trues):
+    ### Converting preds to 0 or 1
+    preds = [1 if preds[i] >= 0.5 else 0 for i in range(len(preds))]
+    ### Calculating accuracy by comparing predictions with true labels
+    acc = [1 if preds[i] == trues[i] else 0 for i in range(len(preds))]
+    ### Summing over all correct predictions
+    acc = np.sum(acc) / len(preds)
+    return (acc * 100)
+    
+def train_one_epoch(train_data_loader):
+    ### Local Parameters
+    epoch_loss = []
+    epoch_acc = []
+    start_time = time.time()
+    
+    ###Iterating over data loader
+    for images, labels in tqdm(train_data_loader):
+        
+        #Loading images and labels to device
+        images = images.to(device)
+        labels = labels.to(device)
+        labels = labels.reshape((labels.shape[0], 1)) # [N, 1] - to match with preds shape
+        
+        #Reseting Gradients
+        optimizer.zero_grad()
+        
+        #Forward
+        preds = model(images)
+        
+        #Calculating Loss
+        _loss = criterion(preds, labels)
+        loss = _loss.item()
+        epoch_loss.append(loss)
+        
+        #Calculating Accuracy
+        acc = accuracy(preds, labels)
+        epoch_acc.append(acc)
+        
+        #Backward
+        _loss.backward()
+        optimizer.step()
+    
+    ###Overall Epoch Results
+    end_time = time.time()
+    total_time = end_time - start_time
+    
+    ###Acc and Loss
+    epoch_loss = np.mean(epoch_loss)
+    epoch_acc = np.mean(epoch_acc)
+    
+    ###Storing results to logs
+    train_logs["loss"].append(epoch_loss)
+    train_logs["accuracy"].append(epoch_acc)
+    train_logs["time"].append(total_time)
+        
+    return epoch_loss, epoch_acc, total_time
+        
+def val_one_epoch(val_data_loader, best_val_acc):
+    
+    ### Local Parameters
+    epoch_loss = []
+    epoch_acc = []
+    start_time = time.time()
+    
+    ###Iterating over data loader
+    for images, labels in val_data_loader:
+        
+        #Loading images and labels to device
+        images = images.to(device)
+        labels = labels.to(device)
+        labels = labels.reshape((labels.shape[0], 1)) # [N, 1] - to match with preds shape
+        
+        #Forward
+        preds = model(images)
+        
+        #Calculating Loss
+        _loss = criterion(preds, labels)
+        loss = _loss.item()
+        epoch_loss.append(loss)
+        
+        #Calculating Accuracy
+        acc = accuracy(preds, labels)
+        epoch_acc.append(acc)
+    
+    ###Overall Epoch Results
+    end_time = time.time()
+    total_time = end_time - start_time
+    
+    ###Acc and Loss
+    epoch_loss = np.mean(epoch_loss)
+    epoch_acc = np.mean(epoch_acc)
+    
+    ###Storing results to logs
+    val_logs["loss"].append(epoch_loss)
+    val_logs["accuracy"].append(epoch_acc)
+    val_logs["time"].append(total_time)
+    
+    ###Saving best model
+    if epoch_acc > best_val_acc:
+        best_val_acc = epoch_acc
+        torch.save(model.state_dict(),"resnet50_best.pth")
+        
+    return epoch_loss, epoch_acc, total_time, best_val_acc
+
+model = resnet50()
+
+# Modifying Head - classifier
+
+model.fc = nn.Sequential(
+    nn.Linear(2048, 1, bias = True),
+    nn.Sigmoid()
+)
+
+# Optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.0001)
+
+# Learning Rate Scheduler
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 5, gamma = 0.5)
+
+#Loss Function
+criterion = nn.BCELoss()
+
+# Logs - Helpful for plotting after training finishes
+train_logs = {"loss" : [], "accuracy" : [], "time" : []}
+val_logs = {"loss" : [], "accuracy" : [], "time" : []}
+
+# Loading model to device
+model.to(device)
+
+# No of epochs 
+epochs = 15
+
+best_val_acc = 0
+if __name__ == '__main__':
+    train_acc = []
+    train_loss = []
+    valid_acc = []
+    valid_loss = []
+    for epoch in tqdm(range(epochs)):
+        
+        ###Training
+        loss, acc, _time = train_one_epoch(train_data_loader)
+        train_acc.append(acc) 
+        train_loss.append(loss)
+        #Print Epoch Details
+        print("\nTraining")
+        print("Epoch {}".format(epoch+1))
+        print("Loss : {}".format(round(loss, 4)))
+        print("Acc : {}".format(round(acc, 4)))
+        print("Time : {}".format(round(_time, 4)))
+        
+        ###Validation
+        loss, acc, _time, best_val_acc = val_one_epoch(val_data_loader, best_val_acc)
+        valid_acc.append(acc) 
+        valid_loss.append(loss)
+        #Print Epoch Details
+        print("\nValidating")
+        print("Epoch {}".format(epoch+1))
+        print("Loss : {}".format(round(loss, 4)))
+        print("Acc : {}".format(round(acc, 4)))
+        print("Time : {}".format(round(_time, 4)))
+        
+    plt.plot(train_acc, label = 'train_acc')
+    plt.plot(valid_acc, label = 'valid_acc')
+    plt.legend()
+    plt.title('Accuracy')
+    plt.savefig('acc_noweight.png')
+    plt.clf()
+    plt.plot(train_loss, label = 'train_loss')
+    plt.plot(valid_loss, label = 'valid_loss')
+    plt.legend()
+    plt.title('Loss')
+    plt.savefig('loss_noweight.png')
+    plt.close()
---
+++ resnet_50_cifar10.py
@@ -0,0 +1,223 @@
+import os
+import cv2
+import time
+import random
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.data import Dataset, DataLoader
+
+import torchvision.transforms as T
+from torchvision.utils import make_grid
+from torchvision.models import resnet50
+
+from sklearn.model_selection import train_test_split
+
+from PIL import Image
+
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from torchvision.datasets import CIFAR10
+
+name = 'resnet50_noweight_cifar10'
+# model = resnet50(weights = 'ResNet50_Weights.DEFAULT')
+model = resnet50(weights = None)
+device = 'cuda'
+def get_train_transform():
+    return T.Compose([
+        T.RandomHorizontalFlip(p=0.5),
+        T.RandomRotation(15),
+        T.RandomCrop(32),
+        T.ToTensor(),
+        T.Normalize((0, 0, 0),(1, 1, 1))
+    ])
+    
+def get_val_transform():
+    return T.Compose([
+        T.ToTensor(),
+        T.Normalize((0, 0, 0),(1, 1, 1))
+    ])
+
+# Download and load the training data
+train_data = CIFAR10(root='./data', train=True, download=True, transform=get_train_transform())
+
+# Download and load the validation data
+val_data = CIFAR10(root='./data', train=False, download=True, transform=get_val_transform())
+train_data_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+val_data_loader = DataLoader(val_data, batch_size=64, shuffle=False)
+
+def accuracy(preds, trues):
+    # Get the predicted classes
+    _, predicted = torch.max(preds, 1)
+    
+    # Calculate the number of correct predictions
+    correct = (predicted == trues).sum().item()
+
+    # Calculate the accuracy
+    acc = correct / len(trues)
+
+    return acc * 100
+    
+def train_one_epoch(train_data_loader):
+    ### Local Parameters
+    epoch_loss = []
+    epoch_acc = []
+    start_time = time.time()
+    
+    ###Iterating over data loader
+    for images, labels in tqdm(train_data_loader):
+        
+        #Loading images and labels to device
+        images = images.to(device)
+        labels = labels.to(device)
+
+        #Reseting Gradients
+        optimizer.zero_grad()
+        
+        #Forward
+        preds = model(images)
+        #Calculating Loss
+        _loss = criterion(preds, labels)
+        loss = _loss.item()
+        epoch_loss.append(loss)
+        
+        #Calculating Accuracy
+        acc = accuracy(preds, labels)
+        epoch_acc.append(acc)
+        
+        #Backward
+        _loss.backward()
+        optimizer.step()
+    
+    ###Overall Epoch Results
+    end_time = time.time()
+    total_time = end_time - start_time
+    
+    ###Acc and Loss
+    epoch_loss = np.mean(epoch_loss)
+    epoch_acc = np.mean(epoch_acc)
+    
+    ###Storing results to logs
+    train_logs["loss"].append(epoch_loss)
+    train_logs["accuracy"].append(epoch_acc)
+    train_logs["time"].append(total_time)
+        
+    return epoch_loss, epoch_acc, total_time
+        
+def val_one_epoch(val_data_loader, best_val_acc):
+    
+    ### Local Parameters
+    epoch_loss = []
+    epoch_acc = []
+    start_time = time.time()
+    
+    ###Iterating over data loader
+    for images, labels in val_data_loader:
+        
+        #Loading images and labels to device
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        #Forward
+        preds = model(images)
+        
+        #Calculating Loss
+        _loss = criterion(preds, labels)
+        loss = _loss.item()
+        epoch_loss.append(loss)
+        
+        #Calculating Accuracy
+        acc = accuracy(preds, labels)
+        epoch_acc.append(acc)
+    
+    ###Overall Epoch Results
+    end_time = time.time()
+    total_time = end_time - start_time
+    
+    ###Acc and Loss
+    epoch_loss = np.mean(epoch_loss)
+    epoch_acc = np.mean(epoch_acc)
+    
+    ###Storing results to logs
+    val_logs["loss"].append(epoch_loss)
+    val_logs["accuracy"].append(epoch_acc)
+    val_logs["time"].append(total_time)
+    
+    ###Saving best model
+    if epoch_acc > best_val_acc:
+        best_val_acc = epoch_acc
+        torch.save(model.state_dict(),f"weight/{name}.pth")
+        
+    return epoch_loss, epoch_acc, total_time, best_val_acc
+
+# Modifying Head - classifier
+model.fc = nn.Sequential(
+    nn.Linear(2048, 10, bias = True),
+    nn.Sigmoid()
+)
+
+# Optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.0001)
+
+# Learning Rate Scheduler
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size = 5, gamma = 0.5)
+
+#Loss Function
+criterion = nn.CrossEntropyLoss()
+
+# Logs - Helpful for plotting after training finishes
+train_logs = {"loss" : [], "accuracy" : [], "time" : []}
+val_logs = {"loss" : [], "accuracy" : [], "time" : []}
+
+# Loading model to device
+model.to(device)
+
+# No of epochs 
+epochs = 15
+
+best_val_acc = 0
+if __name__ == '__main__':
+    train_acc = []
+    train_loss = []
+    valid_acc = []
+    valid_loss = []
+    for epoch in tqdm(range(epochs)):
+        
+        ###Training
+        loss, acc, _time = train_one_epoch(train_data_loader)
+        train_acc.append(acc) 
+        train_loss.append(loss)
+        #Print Epoch Details
+        print("\nTraining")
+        print("Epoch {}".format(epoch+1))
+        print("Loss : {}".format(round(loss, 4)))
+        print("Acc : {}".format(round(acc, 4)))
+        print("Time : {}".format(round(_time, 4)))
+        
+        ###Validation
+        loss, acc, _time, best_val_acc = val_one_epoch(val_data_loader, best_val_acc)
+        valid_acc.append(acc) 
+        valid_loss.append(loss)
+        #Print Epoch Details
+        print("\nValidating")
+        print("Epoch {}".format(epoch+1))
+        print("Loss : {}".format(round(loss, 4)))
+        print("Acc : {}".format(round(acc, 4)))
+        print("Time : {}".format(round(_time, 4)))
+        
+    plt.plot(train_acc, label = 'train_acc')
+    plt.plot(valid_acc, label = 'valid_acc')
+    plt.legend()
+    plt.title(f'Accuracy_{name}')
+    plt.savefig(f'result/acc_{name}.png')
+    plt.clf()
+    plt.plot(train_loss, label = 'train_loss')
+    plt.plot(valid_loss, label = 'valid_loss')
+    plt.legend()
+    plt.title(f'Loss_{name}')
+    plt.savefig(f'result/loss_{name}.png')
+    plt.close()
